e672969b5d0daae403b02f8f626a615173f9b717
AlexKole113 <alexander.koledov@gmail.com>
2021-06-16 19:27:01 +0200

    add react

--- src/components/InputGroup/index.tsx
+++ src/components/InputGroup/index.tsx
@@ -1,5 +1,4 @@
 import style from "@/styles/style.css";
-import preloader from './assets/preloader.gif'
 import {ChangeEvent} from "react";
 
 const InputGroup = ({ placeholder, btnText, formState, action, appState }:{placeholder:string, btnText:string, formState:string, action:(e:ChangeEvent)=> void, appState:{loading:boolean, [key:string]:any} }) => {
@@ -10,7 +9,7 @@ const InputGroup = ({ placeholder, btnText, formState, action, appState }:{place
                 <input onChange={(e)=>{action(e)}} className={style.spinnerUpwInput} type="text" placeholder={placeholder} value={formState} />
 
                 <div className={style.spinnerUpwBtnSlot}>
-                    { ( appState.loading) ? <img className={style.spinnerUpwInputGroupLoader} src={preloader} /> :  <input type="submit" className={style.spinnerUpwFormBtn} value={btnText} /> }
+                    { ( appState.loading) ? <img className={style.spinnerUpwInputGroupLoader} src='assets/preloader.gif' /> :  <input type="submit" className={style.spinnerUpwFormBtn} value={btnText} /> }
                 </div>
             </div>
         </>
