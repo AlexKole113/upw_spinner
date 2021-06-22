b8626384af81a548756f9a09364ecc516d100828
AlexKole113 <alexander.koledov@gmail.com>
2021-06-22 09:33:38 +0200

    check CORS problem

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,62 +8,8 @@ class API {
         console.log('start fetch -')
         return fetch( API.EMBED + `/${gameId}` , {
             method: method,
-           // cache: 'no-cache',
-           credentials: 'omit',
-            // headers: {
-            //     "Content-Type" : "text/plain",
-            // }
+            credentials: 'omit',
         })
-
-        // console.log(gameId,method)
-        // return Promise.resolve({
-        //     "settings": [
-        //         {
-        //             "textFillStyle": "#ffffff",
-        //             "chance": 2.5,
-        //             "text": "50% Discount\nonly today",
-        //             "fillStyle": "#237dc9",
-        //             "coupon": "50%DISCOUNT"
-        //         },
-        //         {
-        //             "coupon": "10$DISCOUNT",
-        //             "text": "10$ Discount",
-        //             "fillStyle": "#03b3ef",
-        //             "chance": 20,
-        //             "textFillStyle": "#ffffff"
-        //         },
-        //         {
-        //             "chance": 125,
-        //             "text": "No Luck",
-        //             "coupon": "",
-        //             "textFillStyle": "#ffffff",
-        //             "fillStyle": "#95c172"
-        //         },
-        //         {
-        //             "chance": 5,
-        //             "text": "25$ Cash",
-        //             "fillStyle": "#f1954c",
-        //             "textFillStyle": "#ffffff",
-        //             "coupon": "25$CASH"
-        //         },
-        //         {
-        //             "text": "No Luck",
-        //             "coupon": "",
-        //             "chance": 125,
-        //             "textFillStyle": "#ffffff",
-        //             "fillStyle": "#f9636e"
-        //         },
-        //         {
-        //             "coupon": "10$CASH",
-        //             "textFillStyle": "#ffffff",
-        //             "text": "10$ Cash",
-        //             "fillStyle": "#9f4966",
-        //             "chance": 10
-        //         }
-        //     ],
-        //     "name": "Spin The Wheel",
-        //     "id": "sampleId"
-        // })
     }
 
     static sendImpression = ( gameId: string, method:string = 'POST' ) => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,28 @@
 import ReactDOM from 'react-dom';
 import App from './components/App'
 
+const embedSrc = 'https://someEmbed/script/there/';
+const cssUrl   = 'dist/assets/css/main.css';
+
+// root
 const root = document.createElement('div');
 root.setAttribute('id','upw-spinner-root' );
+// styles
+const link = document.createElement('link');
+link.setAttribute('rel','stylesheet');
+link.setAttribute('href', cssUrl  );
+
+document.head.append(link)
 document.body.append(root)
+
+const gameID = document.querySelector(`[src^="${embedSrc}"]`)
+    ?.getAttribute('src')
+    ?.split(embedSrc)
+
+console.log(gameID)
+
+
+
 const initialScript = document.querySelector('[data-gameID]');
 
 
