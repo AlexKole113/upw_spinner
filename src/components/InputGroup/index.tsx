0c0c1b275ec652becc6eea7a896c91cebc88bc22
AlexKole113 <alexander.koledov@gmail.com>
2021-06-23 09:10:06 +0200

    testing - image src for preloader in form

--- src/components/InputGroup/index.tsx
+++ src/components/InputGroup/index.tsx
@@ -8,7 +8,7 @@ const InputGroup = ({ placeholder, btnText, formState, action, appState }:{place
             <div className={`${style.inputGroup} ${( formState.error )? style.invalid : ''}`}>
                 <input onChange={(e)=>{action(e)}} className={style.input} type="email" placeholder={placeholder} value={formState.value} />
                 <div className={style.slot}>
-                    { ( appState.loading) ? <img className={style.loader} src='assets/preloader.gif' /> :  <input type="submit" className={style.btn} value={btnText} /> }
+                    { ( appState.loading) ? <img className={style.loader} src='https://cranky-bartik-376bfe.netlify.app/assets/preloader.gif' /> :  <input type="submit" className={style.btn} value={btnText} /> }
                 </div>
             </div>
         </>
